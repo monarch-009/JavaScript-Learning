8371904a6034e1607a9a5455f8ba67b7e51ba40b
monarch-009 <adityasinghmoni@gmail.com>
2024-02-24 15:52:05 +0530

    Variable in js

---
+++ Variables/Variable.js
@@ -0,0 +1,44 @@
+//Example of variable
+// The default data type for a variable in JavaScript is undefined
+
+// fname = "Aditya";
+// lname = "Singh";
+// age = 19;
+// x = null; // we know what is inside it but now it is empty.
+// y = undefined; // undefined means we don't know what is inside it.
+// isYou = true; // This is a boolean type var in which we can Store true/false.
+
+// console.log(fname + " " + lname);
+// console.log(age);
+// console.log(x);
+// console.log(y);
+// console.log(isYou);
+
+// ------------------------------------------------
+
+// var is used before 2015 upto then we don't have let and const in js so at that time  we normally used var to declare variable.
+
+// var name = "Aditya";
+// console.log(name);
+// var name = "Sumit"; // Again we are declare the name variable in same code -> So this is the problem with the use of var
+// console.log(name);
+
+// -------------------------------------------------
+// let is used after 2015,when ES6 lauched. This is the best way to decalre any variable.
+
+
+// let height;
+// console(height); // undefined 
+
+let age = 18;
+console.log(age);
+// age = 20;
+// console.log(age);
+
+// -------------------------------------------------
+
+// const is used to make a variable value unchangeable which is deaclared once cannot we changed at runtime;
+
+const pi = 3.14;
+console.log(pi);
+
