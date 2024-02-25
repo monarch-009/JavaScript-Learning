007c7af43499def0d9f2a55c3703dc4d852cf626
monarch-009 <adityasinghmoni@gmail.com>
2024-02-25 14:09:43 +0530

    Operator in js

---
+++ Operators/operators.js
@@ -0,0 +1,67 @@
+// Operators in js
+// Artimetic Opeartor
+// let a = 22;
+// let b = 10;
+
+// console.log("a = ", a, "& b = ", b);
+// console.log("a + b : ", a + b);
+// console.log("a - b : ", a - b);
+// console.log("a * b : ", a * b);
+// console.log("a / b : ", a / b);
+// console.log("a % b : ", a % b); // Modulus
+// console.log("a ** b : ", a ** b); // Exponentiation
+
+//-------------------------------------------------
+
+// Unary Opeartor
+// a++;  // ++a; // Post/Pre Increment
+// console.log("a++: ", a);
+// a--; // --a; // Post/Pre Decrement
+// console.log("a-- : ", a);
+
+// ------------------------------------------------
+
+// Assignment Operators
+// let a = 5;
+// let b = 6;
+// a += b;
+// console.log("a += b : ", a);
+// a -= b;
+// console.log("a -= b : ", a);
+// a *= b;
+// console.log("a *= b : ", a);
+// a /= b;
+// console.log("a /= b : ", a);
+// a %= b;
+// console.log("a %= b : ", a);
+// a **= b;
+// console.log("a **= b : ", a);
+
+// ------------------------------------------------
+
+// Comparison Operators
+// let a = 5; // number
+// let b = 2;
+
+// console.log("a == b", a == b);
+// console.log("a === b", a === b);
+// console.log("a != b", a != b);
+// console.log("a !== b", a !== b);
+// console.log("a > b", a > b);
+// console.log("a >= b", a >= b);
+// console.log("a < b", a < b);
+// console.log("a <= b", a <= b);
+
+// -----------------------------------------------
+
+// Logical Operator
+
+let a = 9;
+let b = 8;
+
+let cond1 = a > b;
+let cond2 = a < b;
+
+console.log("cond1 && cond2 : ", cond1 && cond2);
+console.log("cond1 || cond2: ", cond1 || cond2);
+console.log("!cond1 : ", !cond1);
