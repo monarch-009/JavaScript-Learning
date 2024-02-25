874e47ac901e0947d8f627329b99e8432e9c9c94
monarch-009 <adityasinghmoni@gmail.com>
2024-02-25 14:10:26 +0530

    Conditional Statement in js

---
+++ Conditional-Statement/condition.js
@@ -0,0 +1,44 @@
+// alert("Please Inspect")
+
+//Example 1:- if-else
+// let age = 25;
+// if (age >= 18) {
+//   console.log("You can vote.");
+// } else {
+//   console.log("You can't vote.");
+// }
+
+//Example 2:-  if-else
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark") {
+//   color = "black";
+// } else {
+//   color = "white";
+// }
+// console.log(color);
+
+// Example 3:-   if-else
+// let num = 15;
+// if (num % 2 === 0) {
+//   console.log(num, " is  an even number.");
+// } else {
+//   console.log(num, " is an odd number");
+// }
+
+// Example 4:- if else-if else
+// let mode = "pink";
+// let color;
+// if (mode === "dark") {
+//   color = "black";
+// } else if (mode === "blue") {
+//   color = "blue";
+// } else if (mode === "pink") {
+//   color = "pink";
+// } else {
+//   color = "white";
+// }
+// console.log(color);
+
+
